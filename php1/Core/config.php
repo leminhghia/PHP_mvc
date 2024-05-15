ccdcf093d0abfcf27a39c3f670411f38eb11d99e
nghia00200 <nghialmpi00200@gmail.com>
2024-05-15 08:39:25 +0700

    mvc

---
+++ php1/Core/config.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "host" => 'localhost',
+    "user" => 'root',
+    "pass" => '',
+    "db_name" => 'php_mvc_db',
+    "subdirectory" => "php1",
+    "app_url" => 'http://localhost/php1',
+    "public_url" => "http://localhost/php1/"
+];
---
+++ php1/Views/errors/404.php
@@ -0,0 +1,1 @@
+<h2 style="color: red;direction: rtl;text-align: center">Page Not Found</h2>
